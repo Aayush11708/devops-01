dd4a3f4179402bd417029a7ba7d2afa907ffd580
Aayush11708 <aayush.bcastudent23.11708@cimage.in>
2025-10-15 12:21:02 +0530

    Update admin.py

--- todos/admin.py
+++ todos/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Todo
 
 
+
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ("title", "completed", "created_at", "updated_at")
